24371ff28371aa31023e812f3bed25bfad1c6ce4
Glenn Powell <glenn@openai.com>
2018-08-21 14:46:58 -0700

    Improving version load/save logic

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ TEMP_DIR = "/tmp/learning"
 @click.option("--seed", "-s", type=int, default=1)
 @click.option("--batch", type=int, default=128)
 @click.option("--evaluate", "evaluate_interval", type=int, default=5)
-@click.option("--version", "-v", type=int, default=0)
+@click.option("--version", "-v", default=None)
 @click.option("--render/--no-render", default=False)
 @click.option("--profile/--no-profile", default=False)
 def main(mode, env_name, dataset_name, policy, episodes, seed, batch, evaluate_interval,
@@ -39,14 +39,25 @@ def main(mode, env_name, dataset_name, policy, episodes, seed, batch, evaluate_i
         return
 
     # prepare log paths
-    load_version = version
-    save_version = load_version + 1
     project = env_name if env_name is not None else dataset_name
-    current_tmp_dir = f"{TEMP_DIR}/{project}/{save_version}"
-    load_path = f"{TEMP_DIR}/{project}/{load_version}/model.ckpt"
-    save_path = f"{current_tmp_dir}/model.ckpt"
-    tensorboard_path = f"{current_tmp_dir}/tensorboard/"
-    profile_path = f"{current_tmp_dir}/profile" if profile else None
+    if version is None:
+        next_version = 1
+        log_dir = f"{TEMP_DIR}/{project}/{next_version}"
+        load_path = None
+        save_path = f"{log_dir}/model.ckpt"
+    elif version.isdigit():
+        version = int(version)
+        next_version = version + 1
+        log_dir = f"{TEMP_DIR}/{project}/{next_version}"
+        load_path = f"{TEMP_DIR}/{project}/{version}/model.ckpt"
+        save_path = f"{log_dir}/model.ckpt"
+    else:
+        next_version = None
+        log_dir = f"{TEMP_DIR}/{project}/{version}"
+        load_path = f"{TEMP_DIR}/{project}/{version}/model.ckpt"
+        save_path = None
+    tensorboard_path = f"{log_dir}/tensorboard/"
+    profile_path = f"{log_dir}/profile" if profile else None
 
     # create policy
     if policy == "cnn":
