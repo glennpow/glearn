90671238223a29f20a0e6efae07b3f0716033e48
Glenn Powell <glenn@openai.com>
2018-09-14 10:39:23 -0700

    Made scripts folder command line calls only

--- glearn/train.py
+++ glearn/train.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python3
-
 import os
-import click
 from glearn.policies import load_policy
 from glearn.utils.config import load_config
 
@@ -22,16 +19,3 @@ def remote_train(config_path):
     config_path = os.path.join(os.path.dirname(__file__), config_path)
 
     train(config_path)
-
-
-@click.command()
-@click.argument("config_path")
-@click.option("--version", "-v", default=None)
-@click.option("--render/--no-render", default=False)
-@click.option("--profile/--no-profile", default=False)
-def main(config_path, version, render, profile):
-    train(config_path, version=version, render=render, profile=profile)
-
-
-if __name__ == "__main__":
-    main()
