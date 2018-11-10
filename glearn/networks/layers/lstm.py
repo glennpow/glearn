Fixed averaged evaluate results

glearn/networks/layers/lstm.py
@@ -82,18 +82,9 @@ class LSTMLayer(NetworkLayer):
         timesteps = self.context.config.get("timesteps", 1)
         vocabulary_size = self.context.dataset.vocabulary.size
 
-        # create output layer
+        # create output layer and convert to batched sequences
         y = self.dense(y, vocabulary_size, self.dropout, tf.nn.softmax)
-        self.references["prob"] = y
-        self.context.set_fetch("prob", y, "evaluate")
-
-        # calculate prediction and accuracy
-        y = tf.argmax(y, axis=1)
-        self.context.set_fetch("arg_max", y, "evaluate")
-        y = tf.cast(y, tf.int32)
-        self.context.set_fetch("int_predict", y, "evaluate")
-
-        self.references["unbatched"] = y
+        y = tf.cast(tf.argmax(y, axis=1), tf.int32)
         y = tf.reshape(y, [batch_size, timesteps])
 
         return y

glearn/networks/losses/sequence.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
 
 
-def sequence_loss(network, outputs, prefix=None, graphs="evaluate"):
+def sequence_loss(network, outputs):
     # get variables
     context = network.context
     batch_size = context.config.get("batch_size", 1)
     timesteps = context.config.get("timesteps", 1)
     # timesteps = context.input.shape[0]
     vocabulary_size = context.dataset.vocabulary.size  # FIXME - ...better way of exposing
-    predict = context.get_fetch("predict", graphs)
+    predict = network.head
     logits = network.get_output_layer().references["Z"]
 
     # calculate loss
@@ -18,7 +18,6 @@ def sequence_loss(network, outputs, prefix=None, graphs="evaluate"):
     sequence_loss = tf.contrib.seq2seq.sequence_loss(logits, outputs, weights,
                                                      average_across_timesteps=False,
                                                      average_across_batch=True)
-    # context.set_fetch(f"sequence_{name}", sequence_loss, graphs)
     loss = tf.reduce_sum(sequence_loss)
 
     # calculate accuracy