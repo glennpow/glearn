2064084c6d6e0205c44d4f90172dabcd38e6d1c5
Glenn Powell <glenn@openai.com>
2019-03-22 18:53:35 -0700

    Fixed VAE nan issue

--- glearn/trainers/variational_autoencoder.py
+++ glearn/trainers/variational_autoencoder.py
@@ -4,6 +4,8 @@ from .generative import GenerativeTrainer
 
 
 class VariationalAutoencoderTrainer(GenerativeTrainer):
+    EPSILON = 1e-5
+
     def __init__(self, config, encoder=None, decoder=None, **kwargs):
         # get basic params
         self.encoder_definition = encoder
@@ -22,7 +24,6 @@ class VariationalAutoencoderTrainer(GenerativeTrainer):
     def build_decoder(self, z, reuse=False):
         self.decoder_network = self.build_generator_network("decoder", self.decoder_definition,
                                                             z=z, reuse=reuse)
-        # decoded = tf.clip_by_value(decoded, 1e-8, 1 - 1e-8)  # was this needed?
         return self.decoder_network.outputs
 
     def build_kl_divergence(self):
@@ -30,7 +31,7 @@ class VariationalAutoencoderTrainer(GenerativeTrainer):
         mu = encoder_distribution.references["mu"]
         sigma = encoder_distribution.references["sigma"]
         kl_divergence = 0.5 * tf.reduce_sum(tf.square(mu) + tf.square(sigma) -
-                                            tf.log(1e-8 + tf.square(sigma)) - 1, 1)
+                                            tf.log(self.EPSILON + tf.square(sigma)) - 1, 1)
         kl_divergence = tf.reduce_mean(kl_divergence)
 
         # summary
@@ -42,6 +43,7 @@ class VariationalAutoencoderTrainer(GenerativeTrainer):
         with tf.variable_scope("VAE_optimize"):
             # losses
             y = decoded
+            y = tf.clip_by_value(y, self.EPSILON, 1 - self.EPSILON)
             x = tf.reshape(x, [-1, np.prod(x.shape[1:])])
             marginal_likelihood = tf.reduce_sum(x * tf.log(y) + (1 - x) * tf.log(1 - y), 1)
             marginal_likelihood = tf.reduce_mean(marginal_likelihood)
