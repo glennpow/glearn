48f76325c85a9a4a88e5d0bc434610b58238b5b2
Glenn Powell <glenn@openai.com>
2019-05-26 08:55:42 -0700

    No more references to RNNViewerMode

--- glearn/viewers/modes/__init__.py
+++ glearn/viewers/modes/__init__.py
@@ -1,7 +1,6 @@
 from glearn.utils.reflection import get_class
 
 from .cnn_viewer_mode import CNNViewerMode  # noqa
-from .rnn_viewer_mode import RNNViewerMode  # noqa
 
 
 def load_viewer_mode(config):
